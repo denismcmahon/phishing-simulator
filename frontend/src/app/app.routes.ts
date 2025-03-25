a4d648164d2e5779c31141ce2a38a800e0e0c463
Denis McMahon <denismcmahon@gmail.com>
2025-03-25 14:55:20 +0000

    frontend login and register logic, plus basic dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login.component';
-import { RegisterComponent } from './pages/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
 ];
---
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar.component';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [NavbarComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+
+}
