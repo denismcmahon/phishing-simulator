646a8f9d461d511ac4f33f5d159a812ae451c206
Denis McMahon <denismcmahon@gmail.com>
2025-03-26 12:58:09 +0000

    getting dashboard data from backend

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../components/navbar.component';
 import { RouterLink } from '@angular/router';
 import { CampaignService, Campaign } from '../../services/campaign.service';
+import { TargetService } from '../../services/target.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,19 +14,29 @@ import { CampaignService, Campaign } from '../../services/campaign.service';
 })
 export class DashboardComponent {
   campaigns: Campaign[] = [];
+  totalCampaigns = 0;
+  totalTargets = 0;
 
-  constructor(private campaignService: CampaignService) {}
+  constructor(
+    private campaignService: CampaignService,
+    private targetService: TargetService
+  ) {}
 
   ngOnInit(): void {
     this.loadCampaigns();
+    this.loadTargetCount();
   }
 
   loadCampaigns(): void {
-    this.campaignService.getCampaigns().subscribe({
-      next: (data) => this.campaigns = data,
-      error: (err) => {
-        console.error('Failed to load campaigns', err);
-      }
+    this.campaignService.getCampaigns().subscribe((data) => {
+      this.campaigns = data;
+      this.totalCampaigns = data.length;
+    });
+  }
+
+  loadTargetCount(): void {
+    this.targetService.getTargets().subscribe((data) => {
+      this.totalTargets = data.length;
     });
   }
 }
